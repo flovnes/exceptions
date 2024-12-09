ac8a4af4abab1b4f66503ec7d60af981506bae90
flovnes <whyflovnes@gmail.com>
2024-12-09 02:21:16 +0200

    part2 fix1

--- part2/mirror.cs
+++ part2/mirror.cs
@@ -1,10 +1,14 @@
 ﻿using System.Text.RegularExpressions;
 class Mirror {
     static void Main() {
-        Console.WriteLine("enter dir path:");
-        string? directoryPath = Console.ReadLine();
-        directoryPath = directoryPath==null || directoryPath.Trim().Length==0 ? Directory.GetCurrentDirectory() : directoryPath;
-        string[] files = Directory.GetFiles(directoryPath);
+        Console.WriteLine("Enter folder name (e.g. src/textures/grass):");
+        string directoryPath = Console.ReadLine();
+        directoryPath = directoryPath.Trim().Length==0 ? Directory.GetCurrentDirectory() : directoryPath;
+        string[] files = null;
+        try {
+            files = Directory.GetFiles(Directory.GetCurrentDirectory() + "\\" + directoryPath);
+        } 
+        catch { Console.WriteLine("! bad path"); }
 
         foreach (string fileName in files) {
             try {
@@ -13,14 +17,12 @@ class Mirror {
                 string fileNameNoExt;
                 int i;
                 fileNameNoExt = (i=fileName.LastIndexOf('.')) == -1 ? fileName : fileName[..i];
-                string newName = Path.Combine(directoryPath, $"{fileNameNoExt}-mirrored.gif");
-
+                string newName = directoryPath + $"{fileNameNoExt}-mirrored.gif";
                 image.Save(newName, System.Drawing.Imaging.ImageFormat.Gif);
-            }
-            catch (Exception ex) {
-                Regex regexExtForImage = new("^((bmp)|(gif)|(tiff?)|(jpe?g)|(png))$", RegexOptions.IgnoreCase);
+            } catch {
+                Regex regexExtForImage = new(@"^.*\.((bmp)|(gif)|(tiff?)|(jpe?g)|(png))$", RegexOptions.IgnoreCase);
                 if (regexExtForImage.IsMatch(fileName)) {
-                    MessageBox.Show($"{fileName} is not an image: {ex.Message}", "try again");
+                    MessageBox.Show($"{fileName}\n is not an image!!!!", "caption", buttons: MessageBoxButtons.YesNoCancel);
                 }
             }
         }
